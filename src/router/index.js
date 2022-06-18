18a00a935b1fdca88af5c9fcf847015a6a112e97
hehongd <635418133@qq.com>
2022-06-18 22:47:48 +0800

    888

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [{
+    path: '/login',
+    name: 'Login',
+    component: () =>
+        import ('../views/Login')
+}]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
---
+++ src/views/Login/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="login-contanier">
+    <div class="title-contanier">
+        <h3>用户登录</h3>
+    </div>
+    <el-form :model="form" ref="formRef" :rules="formRules" class="form-contanier">
+      <el-form-item prop="uname">
+        <el-icon class="icon-contanier">
+            <User />
+        </el-icon>
+        <el-input v-model="form.uname" />
+      </el-form-item>
+      <el-form-item prop="upwd">
+        <el-icon class="icon-contanier">
+            <Unlock />
+        </el-icon>
+        <el-input v-model="form.upwd" :type="passwordType" />
+        <el-icon class="eye-contanier" @click="handleIcon">
+            <Watermelon v-show="passwordType ==='password'? true : false" />
+            <View v-show="passwordType ==='text'? true : false"/>
+        </el-icon>
+      </el-form-item>
+      <el-button type="primary" class="btnSubmit" @click="handleLogin">确认</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { User,Unlock,Watermelon,View } from '@element-plus/icons-vue'
+const form = ref({
+  uname: "",
+  upwd: "",
+});
+
+const formRules = ref({
+    uname:[
+        { required: true, message: 'Please input Activity name', trigger: 'blur' },
+        { min: 3, max: 12, message: 'Length should be 3 to 12', trigger: 'blur' },
+    ],
+    upwd:[
+        { required: true, message: 'Please input Activity password', trigger: 'blur' },
+        { min: 3, max: 12, message: 'Length should be 3 to 12', trigger: 'blur' },
+    ]
+})
+
+const formRef = ref(null)
+// 点击确认
+const handleLogin = () => {
+    formRef.value.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+    }
+  })
+}
+
+const passwordType = ref('password')
+// 点击眼睛
+const handleIcon = () => {
+    if(passwordType.value === 'password'){
+        passwordType.value = "text"
+    }else{
+        passwordType.value = 'password'
+    }
+}
+</script>
+<style lang="scss" scoped>
+.login-contanier {
+    width: 400px;
+    height: 400px;
+    background: #FFF;
+    border-radius: 10px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    padding: 20px 50px;
+    box-sizing: border-box;
+    .title-contanier {
+        height: 50px;
+        position: absolute;
+        left: 50%;
+        top: 100px;
+        transform: translate(-50%,-50%);
+        font-size: 26px;
+    }
+    .form-contanier {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        .icon-contanier {
+            position: absolute;
+            color: #FFF;
+            left:10px;
+            z-index: 666;
+
+        }
+        .eye-contanier {
+            position: absolute;
+            color: #FFF;
+            right:10px;
+            z-index: 666;
+        }
+        .btnSubmit {
+            width: 100%;
+        }
+    }
+}
+
+::v-deep .el-input__wrapper  {
+    background: #4e4141;
+    height: 30px;
+    width: 200px;
+    padding: 0 25px;
+    box-sizing: border-box;
+    input {
+        color: #FFF;
+    }
+}
+</style>
